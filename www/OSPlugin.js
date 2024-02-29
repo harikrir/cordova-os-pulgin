9fdbf8ef9f6d0703e34b2b93485f20f34510bdf8
harikrir <hkrishnan254@gmail.com>
2024-03-01 00:11:26 +0300

    first commit

---
+++ www/OSPlugin.js
@@ -0,0 +1,9 @@
+var exec = require('cordova/exec');
+
+let OSPlugin = {
+    showToast: (message, duration, successCallback, errorCallback) => {
+        exec(successCallback, errorCallback, 'OSPlugin', 'showToast', [message, duration]);
+    }
+};
+
+module.exports = OSPlugin;
